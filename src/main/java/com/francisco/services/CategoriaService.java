3035ce6ddddea243882fbbdcae035065b12c6e59
Francisco Viana <franciscoviana862@gmail.com>
2019-07-15 21:33:43 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/francisco/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.francisco.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.francisco.domain.Categoria;
+import com.francisco.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
