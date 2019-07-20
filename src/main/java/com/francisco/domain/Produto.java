dfb1fd21ffe6a2c4bca048f67760cb1a7f6608b8
Francisco Viana <franciscoviana862@gmail.com>
2019-07-20 16:53:48 -0300

    Atualizacao: utilizando somente @JsonIgnore

--- src/main/java/com/francisco/domain/Produto.java
+++ src/main/java/com/francisco/domain/Produto.java
@@ -1,5 +1,8 @@
 package com.francisco.domain;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -12,12 +15,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
 
 @Entity
 public class Produto implements Serializable {
@@ -29,7 +27,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "produto_id"),
